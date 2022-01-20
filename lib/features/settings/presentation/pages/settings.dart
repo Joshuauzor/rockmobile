f3c489fa392d31e194c282e075a0c49c3bdc1f64
joshuauzor <joshuauzor10@gmail.com>
2022-01-20 16:02:24 +0100

    settings view fixes

--- lib/features/settings/presentation/pages/settings.dart
+++ lib/features/settings/presentation/pages/settings.dart
@@ -23,133 +23,146 @@ class _SettingsState extends State<Settings> {
             viewModelBuilder: () => SettingsViewModel(),
             builder: (context, model, child) {
               return SafeArea(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const SizedBox(
-                            width: 121,
-                          ),
-                          const TitleText(
-                            'Settings',
-                            fontSize: 24,
-                            textAlign: TextAlign.center,
-                          ),
-                          Image.asset(AppAssets.generallogo),
-                        ],
-                      ),
-                      const Gap(51),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 21),
-                        child: Divider(),
-                      ),
-                      const Gap(66),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 46,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(
+                          width: 121,
                         ),
+                        const TitleText(
+                          'Settings',
+                          fontSize: 24,
+                          textAlign: TextAlign.center,
+                        ),
+                        Image.asset(AppAssets.generallogo),
+                      ],
+                    ),
+                    const Gap(51),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 21),
+                      child: Divider(),
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
                         child: Column(
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 46,
+                              ),
+                              child: Column(
+                                children: [
+                                  const Gap(66),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const TitleText(
+                                        'Change password',
+                                        fontSize: 20,
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      SvgPicture.asset(AppAssets.arrowright)
+                                    ],
+                                  ),
+                                  const Gap(37),
+                                  TouchableOpacity(
+                                    onTap: () => Navigator.pushNamed(
+                                      context,
+                                      Routes.about,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const TitleText(
+                                          'About us',
+                                          fontSize: 20,
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SvgPicture.asset(AppAssets.arrowright)
+                                      ],
+                                    ),
+                                  ),
+                                  const Gap(37),
+                                  TouchableOpacity(
+                                    onTap: () => Navigator.pushNamed(
+                                      context,
+                                      Routes.privacyPolicy,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const TitleText(
+                                          'Privacy policy',
+                                          fontSize: 20,
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SvgPicture.asset(AppAssets.arrowright)
+                                      ],
+                                    ),
+                                  ),
+                                  const Gap(37),
+                                  TouchableOpacity(
+                                    onTap: () => Navigator.pushNamed(
+                                      context,
+                                      Routes.terms,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const TitleText(
+                                          'Terms and conditions',
+                                          fontSize: 20,
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SvgPicture.asset(AppAssets.arrowright)
+                                      ],
+                                    ),
+                                  ),
+                                  const Gap(65),
+                                ],
+                              ),
+                            ),
+                            Column(
                               children: [
-                                const TitleText(
-                                  'Change password',
-                                  fontSize: 20,
-                                  textAlign: TextAlign.left,
+                                const Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 21),
+                                  child: Divider(),
                                 ),
-                                SvgPicture.asset(AppAssets.arrowright)
+                                const Gap(39),
+                                TouchableOpacity(
+                                  onTap: () async => {
+                                    await model.logout(context),
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 47),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const TitleText(
+                                          'Logout',
+                                          fontSize: 20,
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SvgPicture.asset(AppAssets.logout)
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ],
                             ),
-                            const Gap(37),
-                            TouchableOpacity(
-                              onTap: () => Navigator.pushNamed(
-                                context,
-                                Routes.about,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const TitleText(
-                                    'About us',
-                                    fontSize: 20,
-                                    textAlign: TextAlign.left,
-                                  ),
-                                  SvgPicture.asset(AppAssets.arrowright)
-                                ],
-                              ),
-                            ),
-                            const Gap(37),
-                            TouchableOpacity(
-                              onTap: () => Navigator.pushNamed(
-                                context,
-                                Routes.privacyPolicy,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const TitleText(
-                                    'Privacy policy',
-                                    fontSize: 20,
-                                    textAlign: TextAlign.left,
-                                  ),
-                                  SvgPicture.asset(AppAssets.arrowright)
-                                ],
-                              ),
-                            ),
-                            const Gap(37),
-                            TouchableOpacity(
-                              onTap: () => Navigator.pushNamed(
-                                context,
-                                Routes.terms,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const TitleText(
-                                    'Terms and conditions',
-                                    fontSize: 20,
-                                    textAlign: TextAlign.left,
-                                  ),
-                                  SvgPicture.asset(AppAssets.arrowright)
-                                ],
-                              ),
-                            ),
-                            const Gap(65),
                           ],
                         ),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 21),
-                        child: Divider(),
-                      ),
-                      const Gap(39),
-                      TouchableOpacity(
-                        onTap: () async => {
-                          await model.logout(context),
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 47),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const TitleText(
-                                'Logout',
-                                fontSize: 20,
-                                textAlign: TextAlign.left,
-                              ),
-                              SvgPicture.asset(AppAssets.logout)
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               );
             }));
