e7622a0ed6a826301677f87dcbf5c01cb15d5409
joshuauzor <joshuauzor10@gmail.com>
2022-02-11 16:39:05 +0100

    media

--- lib/features/media/presentation/pages/video.dart
+++ lib/features/media/presentation/pages/video.dart
@@ -1,11 +1,11 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:gap/gap.dart';
 import 'package:rockapp/app/styles/colors.dart';
 import 'package:rockapp/app/styles/text_styles.dart';
+import 'package:rockapp/app/views/widgets/video_media.dart';
 import 'package:rockapp/app/views/widgets/widgets.dart';
-import 'package:rockapp/core/constant/constant.dart';
 import 'package:rockapp/core/extensions/string_extensions.dart';
 import 'package:rockapp/view_models/home/music_viewmodel.dart';
 import 'package:stacked/stacked.dart';
@@ -86,94 +86,22 @@ class _MediaLibraryState extends State<MediaLibrary> {
                           SearchBar(searchController: _searchController),
                           const Gap(26),
                           _musicList.isNotEmpty
-                              ? Row(
-                                  children: [
-                                    Container(
-                                      width: 170,
-                                      height: 220,
-                                      decoration: const BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(16),
+                              ? StaggeredGrid.count(
+                                  crossAxisCount: 2,
+                                  children: model.audioMusic!
+                                      .map(
+                                        (e) => Row(
+                                          children: [
+                                            VideoMedia(
+                                              image:
+                                                  'https://rockapostolate.org/public/users/previewImage/1640170305_8b48f35daecadb708317.jpg',
+                                              title: e.title,
+                                              author: 'Richmond Diala',
+                                            )
+                                          ],
                                         ),
-                                        color: AppColors.white,
-                                        boxShadow: [
-                                          BoxShadow(
-                                            offset: Offset(0, 8),
-                                            blurRadius: 8,
-                                            spreadRadius: 0,
-                                            color: AppColors.homeMenuBox,
-                                          )
-                                        ],
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Expanded(
-                                            child: ClipRRect(
-                                              borderRadius:
-                                                  const BorderRadius.only(
-                                                topLeft: Radius.circular(16),
-                                                topRight: Radius.circular(16),
-                                              ),
-                                              child: CachedNetworkImage(
-                                                imageUrl:
-                                                    'https://rockapostolate.org/public/users/previewImage/1640170305_8b48f35daecadb708317.jpg',
-                                                imageBuilder:
-                                                    (context, imageProvider) {
-                                                  return Container(
-                                                    decoration: BoxDecoration(
-                                                      shape: BoxShape.rectangle,
-                                                      image: DecorationImage(
-                                                        image: imageProvider,
-                                                        fit: BoxFit.cover,
-                                                      ),
-                                                    ),
-                                                  );
-                                                },
-                                                placeholder: (context, url) =>
-                                                    Image.asset(
-                                                  AppAssets.music,
-                                                  color: AppColors.black,
-                                                ),
-                                                errorWidget:
-                                                    (context, url, error) =>
-                                                        Image.asset(
-                                                  AppAssets.exodus,
-                                                  color: AppColors.black,
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                          const Gap(3),
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(left: 7),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: const [
-                                                TitleText(
-                                                  'Voice Of God',
-                                                  fontSize: 11,
-                                                  fontWeight: FontWeight.w500,
-                                                  color: AppColors.black,
-                                                ),
-                                                Gap(3),
-                                                BodyText(
-                                                  'BY Christ Gospel Church',
-                                                  fontSize: 6,
-                                                  fontWeight: FontWeight.w300,
-                                                  color: AppColors.black,
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                          const Gap(17),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
+                                      )
+                                      .toList(),
                                 )
                               : const BodyText('No Music available'),
                         ],
